1f44b614a97148e545792ca83f9c8c2ebd974b5f
Hira-Yuki <cjsdlf44@gmail.com>
2023-12-03 17:48:25 +0900

    [learn] Learning redux middleware 리덕스 복습과 함께 미들웨어를 만들어 봅니다. 부트캠프에서 가볍게 핥고만 지나갔던 개념을 다지도록 합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { useState } from 'react';
+import { RootState } from './reducers';
 
 type Props = {
-  value: number;
   onIncrement: () => void;
   onDecrement: () => void;
 }
 
-function App({ value, onIncrement, onDecrement }: Props) {
+function App({ onIncrement, onDecrement }: Props) {
+  const disoatch = useDispatch()
+  const todos: string[] = useSelector((state: RootState) => state.todos)
+  const counter = useSelector((state: RootState) => state.counter)
+  const [todoValue, setTodoValue] = useState("")
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value)
+  }
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    disoatch({type:"ADD_TODO", text: todoValue})
+    setTodoValue("");
+  }
+
   return (
     <div className="App">
-      Clicked: {value} times
+      Clicked: {counter} times
       {" "}
       <button onClick={onIncrement}>
         +
@@ -19,6 +34,15 @@ function App({ value, onIncrement, onDecrement }: Props) {
       <button onClick={onDecrement}>
         -
       </button>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+
+      </form>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import counter from './reducers';
-import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store)
+  console.log("action", action)
+  next(action)
+}
+
+const middleware = applyMiddleware(loggerMiddleware)
+const store = createStore(rootReducer, middleware);
 
 const render = () => root.render(
   <React.StrictMode>
-    <App
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-    />
+    <Provider store={store}>
+      <App
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      />
+    </Provider>
   </React.StrictMode>
 );
 render();
